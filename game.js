d9254223b67519302df3848347ce02ca8c50ffe8
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-09 09:58:58 -0500

    commented draw_board()'s body

--- game.js
+++ game.js
@@ -16,22 +16,22 @@ let ties = 0;
 
 // updates the DOM based on the board array
 function draw_board() {
-	for (let i = 0; i < 6; i++) {
-		for (let j = 0; j < 7; j++) {
-			let tile = document.getElementById(`tile-${i}-${j}`);
-			let marker = board[i][j];
+	// for (let i = 0; i < 6; i++) {
+	// 	for (let j = 0; j < 7; j++) {
+	// 		let tile = document.getElementById(`tile-${i}-${j}`);
+	// 		let marker = board[i][j];
 
-			// not optimized
-			// unnecessarily modifying classes at every render
-			// but ok for now
-			if (marker == ' ') {
-				tile.classList.remove('clicked');
-			} else {
-				tile.classList.add('clicked');
-			}
-			tile.innerHTML = marker;
-		}
-	}
+	// 		// not optimized
+	// 		// unnecessarily modifying classes at every render
+	// 		// but ok for now
+	// 		if (marker == ' ') {
+	// 			tile.classList.remove('clicked');
+	// 		} else {
+	// 			tile.classList.add('clicked');
+	// 		}
+	// 		tile.innerHTML = marker;
+	// 	}
+	// }
 }
 
 // returns true if col is full, or false otherwise
