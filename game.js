d0fe04465cbdf23a48921396e5f341d5a239d5ae
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-09 09:51:57 -0500

    formatted indentations with tabs

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 "use strict";
 
 let board = [
-    [" ", " ", " ", " ", " ", " "], // column one
-    [" ", " ", " ", " ", " ", " "], // column two
-    [" ", " ", " ", " ", " ", " "], // column three
-    [" ", " ", " ", " ", " ", " "], // column four
-    [" ", " ", " ", " ", " ", " "], // column five
-    [" ", " ", " ", " ", " ", " "], // column six
-    [" ", " ", " ", " ", " ", " "]  // column seven
+	[" ", " ", " ", " ", " ", " "], // column one
+	[" ", " ", " ", " ", " ", " "], // column two
+	[" ", " ", " ", " ", " ", " "], // column three
+	[" ", " ", " ", " ", " ", " "], // column four
+	[" ", " ", " ", " ", " ", " "], // column five
+	[" ", " ", " ", " ", " ", " "], // column six
+	[" ", " ", " ", " ", " ", " "]  // column seven
 ];
 
 let wins = 0;
@@ -16,22 +16,22 @@ let ties = 0;
 
 // updates the DOM based on the board array
 function draw_board() {
-    // for (let i = 0; i < 6; i++) {
-    //     for (let j = 0; j < 7; j++) {
-    //         let tile = document.getElementById(`tile-${i}-${j}`);
-    //         let marker = board[i][j];
+	for (let i = 0; i < 6; i++) {
+		for (let j = 0; j < 7; j++) {
+			let tile = document.getElementById(`tile-${i}-${j}`);
+			let marker = board[i][j];
 
-    //         // not optimized
-    //         // unnecessarily modifying classes at every render
-    //         // but ok for now
-    //         if (marker == ' ') {
-    //             tile.classList.remove('clicked');
-    //         } else {
-    //             tile.classList.add('clicked');
-    //         }
-    //         tile.innerHTML = marker;
-    //     }
-    // }
+			// not optimized
+			// unnecessarily modifying classes at every render
+			// but ok for now
+			if (marker == ' ') {
+				tile.classList.remove('clicked');
+			} else {
+				tile.classList.add('clicked');
+			}
+			tile.innerHTML = marker;
+		}
+	}
 }
 
 // returns true if col is full, or false otherwise
@@ -51,16 +51,16 @@ function aiTurn() {
 
 // clears the board
 function reset() {
-    board = [
-        [" ", " ", " ", " ", " ", " "], // column one
-        [" ", " ", " ", " ", " ", " "], // column two
-        [" ", " ", " ", " ", " ", " "], // column three
-        [" ", " ", " ", " ", " ", " "], // column four
-        [" ", " ", " ", " ", " ", " "], // column five
-        [" ", " ", " ", " ", " ", " "], // column six
-        [" ", " ", " ", " ", " ", " "]  // column seven
-    ];
-    draw_board();
+	board = [
+		[" ", " ", " ", " ", " ", " "], // column one
+		[" ", " ", " ", " ", " ", " "], // column two
+		[" ", " ", " ", " ", " ", " "], // column three
+		[" ", " ", " ", " ", " ", " "], // column four
+		[" ", " ", " ", " ", " ", " "], // column five
+		[" ", " ", " ", " ", " ", " "], // column six
+		[" ", " ", " ", " ", " ", " "]  // column seven
+	];
+	draw_board();
 }
 
 // starts the game
