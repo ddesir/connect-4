46f1abeec6a587b34a619f6a7bed92275afcbbaa
Henotis <Henryli@Mec.science>
2021-01-09 23:55:07 -0500

    when player clicks, the players piece(X) drops to the bottom

--- game.js
+++ game.js
@@ -1,47 +1,56 @@
 "use strict";
-
-const ROWS = 6;
-const COLUMNS = 7;
-
 let board = [
-	[], // column one
-	[], // column two
-	[], // column three
-	[], // column four
-	[], // column five
-	[], // column six
-	[]  // column seven
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column one
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column two
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column three
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column four
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column five
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column six
+	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column seven
 ];
 
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
-//This will update WLT whenever the user or AI enters a piece
+function draw_board(){
+    for(let i = 0; i < 7; i++){
+		console.log(i)
+	for(let j = 0; j < 6; j++){
+		console.log(j)
+	    let tile = document.getElementById(`tile-${i}-${j}`);
+	    let marker = board[i][j];
+
+	    // not optimized
+	    // unnecessarily modifying classes at every render
+	    // but ok for now
+	    if(marker == ' '){
+		tile.classList.remove('clicked');
+	    }else{
+		tile.classList.add('clicked');
+	    }
+	    tile.innerHTML = marker;
+	}
+    }
+}
+
+function choose(x, y){
+    let tile = board[x][y];
+    if(tile != ' '){
+	// do nothing
+	// rely on css to provide user visual feedback
+    }else{
+	// assume the player is always X
+	board[x].pop(" ")
+	board[x].unshift("X");
+	draw_board();
+    }
+}
+
 function wlt() {
 	document.getElementById("wins").innerHTML = "Wins: " + wins;
 	document.getElementById("losses").innerHTML = "Losses : " + losses;
 	document.getElementById("ties").innerHTML = "Ties : " + ties;
-}
-
-// updates the DOM based on the board
-function drawBoard() {
-	// for (let i = 0; i < 6; i++) {
-	// 	for (let j = 0; j < 7; j++) {
-	// 		let tile = document.getElementById(`tile-${i}-${j}`);
-	// 		let marker = board[i][j];
-
-	// 		// not optimized
-	// 		// unnecessarily modifying classes at every render
-	// 		// but ok for now
-	// 		if (marker == ' ') {
-	// 			tile.classList.remove('clicked');
-	// 		} else {
-	// 			tile.classList.add('clicked');
-	// 		}
-	// 		tile.innerHTML = marker;
-	// 	}
-	// }
 }
 
 // returns true if col is full
@@ -67,28 +76,36 @@ function isBoardFull() {
 
 // processes user move
 function userTurn() {
-
+	//if push x or x, change table class to yellow or red
 	wlt();
 }
 
 // processes AI move
 function aiTurn() {
-
+	//if push x or x, change table class to yellow or red
 	wlt();
 }
 
 // clears the board
+// function reset(){
+//     board = [
+// 	[' ', ' ', ' '],
+// 	[' ', ' ', ' '],
+// 	[' ', ' ', ' ']
+//     ];
+//     draw_board();
+// }
 function reset() {
 	board = [
-		[], // column one
-		[], // column two
-		[], // column three
-		[], // column four
-		[], // column five
-		[], // column six
-		[]  // column seven
+		[' ', ' ', ' ', ' ', ' ', ' '], // column one
+		[' ', ' ', ' ', ' ', ' ', ' '], // column two
+		[' ', ' ', ' ', ' ', ' ', ' '], // column three
+		[' ', ' ', ' ', ' ', ' ', ' '], // column four
+		[' ', ' ', ' ', ' ', ' ', ' '], // column five
+		[' ', ' ', ' ', ' ', ' ', ' '], // column six
+		[' ', ' ', ' ', ' ', ' ', ' '] // column seven
 	];
-	drawBoard();
+	draw_board();
 }
 
 // starts the game
