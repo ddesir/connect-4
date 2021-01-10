f57d78693fb81f48f70e1dd24419d9a23725c8af
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-10 10:51:36 -0500

    formatted all files

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 "use strict";
 
-//this board is rotated 90 degrees clockwise to players
+const ROWS = 6;
+const COLUMNS = 7;
+
+// this board is rotated 90 degrees clockwise to players
 let board = [
 	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column one
 	[' ', ' ', ' ', ' ', ' ', ' ', ' '], // column two
@@ -15,46 +18,47 @@ let wins = 0;
 let losses = 0;
 let ties = 0;
 
-function draw_board(){
-    for(let i = 0; i < 7; i++){
-		for(let j = 0; j < 6; j++){
-	    	let tile = document.getElementById(`tile-${i}-${j}`);
+// updates the document based on the board
+function drawBoard() {
+	for (let i = 0; i < COLUMNS; i++) {
+		for (let j = 0; j < ROWS; j++) {
+			let tile = document.getElementById(`tile-${i}-${j}`);
 			let marker = board[i][j];
-			
-			if(marker == ' '){
+
+			if (marker == ' ') {
 				tile.classList.remove('clicked');
 			}
-			else{
+			else {
 				tile.classList.add('clicked');
-	    	}
-	    tile.innerHTML = marker;
+			}
+			tile.innerHTML = marker;
 		}
-    }
+	}
 }
 
-//win condition should go here
-function fourInRow(){
-	
+// win condition should go here
+function fourInRow() {
+
 }
 
-//this function will drop the player's piece to the bottom of each column
-function choose(x, y){
-    let tile = board[x][y];
-    if(tile != ' '){
-	// do nothing
-	// rely on css to provide user visual feedback
-    }else{
-	// assume the player is always X
-	//pop removes the last element in array so array stays at 6
-	board[x].pop(" ");
-	//unshift adds X to the first element of the array
-	board[x].unshift("X");
-	//updates the board
-	draw_board();
-    }
+// this function will drop the player's piece to the bottom of each column
+function choose(x, y) {
+	let tile = board[x][y];
+	if (tile != ' ') {
+		// do nothing
+		// rely on css to provide user visual feedback
+	} else {
+		// assume the player is always X
+		// pop removes the last element in array so array stays at 6
+		board[x].pop(" ");
+		// unshift adds X to the first element of the array
+		board[x].unshift("X");
+		// updates the board
+		drawBoard();
+	}
 }
 
-//this should be put under win/loss conditions
+// this should be put under win/loss conditions
 function wlt() {
 	document.getElementById("win").innerHTML = "Wins: " + wins;
 	document.getElementById("loss").innerHTML = "Losses : " + losses;
@@ -84,23 +88,15 @@ function isBoardFull() {
 
 // processes user move
 function userTurn() {
-	//if push x or x, change table class to yellow or red
+	// if push x or x, change table class to yellow or red
 }
 
 // processes AI move
 function aiTurn() {
-	//if push x or x, change table class to yellow or red
+	// if push x or x, change table class to yellow or red
 }
 
 // clears the board
-// function reset(){
-//     board = [
-// 	[' ', ' ', ' '],
-// 	[' ', ' ', ' '],
-// 	[' ', ' ', ' ']
-//     ];
-//     draw_board();
-// }
 function reset() {
 	board = [
 		[' ', ' ', ' ', ' ', ' ', ' '], // column one
@@ -111,7 +107,7 @@ function reset() {
 		[' ', ' ', ' ', ' ', ' ', ' '], // column six
 		[' ', ' ', ' ', ' ', ' ', ' '] // column seven
 	];
-	draw_board();
+	drawBoard();
 }
 
 // starts the game
